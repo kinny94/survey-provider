450eb4cffe328c3badbe064488e9eb0283f3ffbd
kinny94 <ad.sam007@gmail.com>
2018-01-22 22:35:15 -0500

    Added googleStrategy code

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require( 'express' );
 const passport = require( 'passport' );
 const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+const keys = require( './config/keys' ); 
 
 const app = express();
-passport.use( new GoogleStrategy() );
+passport.use( 
+        new GoogleStrategy( 
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        
+        ( accessToken ) => {
+             console.log( accessToken );
+        }
+    ) 
+);
 
 // - Client Id
 // - Client Secret
