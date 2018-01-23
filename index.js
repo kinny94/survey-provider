ed8286e061499a6014e2e2878849da7ce7908d7d
kinny94 <ad.sam007@gmail.com>
2018-01-22 22:25:34 -0500

    Added client id in a separate file

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
 const app = express();
 passport.use( new GoogleStrategy() );
 
-//378318128980-gsdgnccn3e8rgl1j5ao3p1vmo75oh63s.apps.googleusercontent.com - Client Id
-// PTBMuADdOWojRj5R0zXRITum - Client Secret
+// - Client Id
+// - Client Secret
 
 const PORT = process.env.PORT || 5000;
 app.listen( PORT );
