bc4cde1f5cf034e01a5fe24f031d1f03d821c393
kinny94 <ad.sam007@gmail.com>
2018-01-25 15:52:54 -0500

    Adding users with mongoose

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require( 'passport' );
 const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+const mongoose = require( 'mongoose' ); 
 const keys = require( '../config/keys' ); 
+
+const User = mongoose.model( 'users' );
 
 passport.use( 
     new GoogleStrategy( 
@@ -11,11 +14,10 @@ passport.use(
     },
     
     ( accessToken, refreshToken, profile, done ) => {
-        console.log( 'access token: ', accessToken );
-        console.log( 'refresh token: ', refreshToken );
-        console.log( 'profile: ', profile._raw );
-    }
-) 
+        new User({
+            googleId: profile.id
+        }).save();
+    })  
 );
 
     
