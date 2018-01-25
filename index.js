ecd9d19977b05de4818b04d4ff463473768a31df
kinny94 <ad.sam007@gmail.com>
2018-01-25 14:42:59 -0500

    added and connected to mongoose

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require( 'express' );
+const mongoose = require( 'mongoose' );
+const keys = require( './config/keys' );
 require('./services/passport');
 
+mongoose.connect( keys.mongoURI, () => {
+    console.log("connected!!");
+} );
 
 const app = express();
 
 // middlewares
-// var authRoutes = require('./routes/authRoutes') and then authRoutes( app ) also works;
+// var authRoutes = require('./routes/authRoutes') and then authRoutes( app ) also works        ;
 require('./routes/authRoutes')( app );
 
 const PORT = process.env.PORT || 5000;
