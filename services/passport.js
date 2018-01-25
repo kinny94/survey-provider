79a9db9b323fc6073ab3b7c73a72370b796e4c73
kinny94 <ad.sam007@gmail.com>
2018-01-25 15:59:20 -0500

    Handling duplicate users

--- services/passport.js
+++ services/passport.js
@@ -14,9 +14,19 @@ passport.use(
     },
     
     ( accessToken, refreshToken, profile, done ) => {
-        new User({
-            googleId: profile.id
-        }).save();
+
+        User.findOne({
+            googleId: profile.id    
+        }).then(( existingUser ) => {
+            if( existingUser ){
+                // we already have a record with this googleID
+                console.log( "User id exists" );
+            }else{
+                new User({
+                    googleId: profile.id
+                }).save();
+            }
+        })
     })  
 );
 
