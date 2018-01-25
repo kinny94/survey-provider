6c730eda81273dd02181737a1bacb204239c7eeb
kinny94 <ad.sam007@gmail.com>
2018-01-25 13:48:13 -0500

    Refactored the code

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
 const express = require( 'express' );
-const passport = require( 'passport' );
-const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
-const keys = require( './config/keys' ); 
+require('./services/passport');
+
 
 const app = express();
 
 // middlewares
-passport.use( 
-        new GoogleStrategy( 
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },
-        
-        ( accessToken, refreshToken, profile, done ) => {
-            console.log( 'access token: ', accessToken );
-            console.log( 'refresh token: ', refreshToken );
-            console.log( 'profile: ', profile._raw );
-        }
-    ) 
-);
-
-app.get( '/auth/google', passport.authenticate( 'google', {
-        scope: [ 'profile', 'email' ]
-    }) 
-);
-
-
-app.get( '/auth/google/callback', passport.authenticate( 'google', {
-
-    }) 
-);
+// var authRoutes = require('./routes/authRoutes') and then authRoutes( app ) also works;
+require('./routes/authRoutes')( app );
 
 const PORT = process.env.PORT || 5000;
 app.listen( PORT );
