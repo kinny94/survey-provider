1439d53b417a4f1ee70221e97e4bda54bee0a7f0
kinny94 <ad.sam007@gmail.com>
2018-01-26 10:10:54 -0500

    managing authentication with cookies

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require( 'express' );
 const mongoose = require( 'mongoose' );
 const keys = require( './config/keys' );
+const passport = require( 'passport' );
+const cookieSession = require( 'cookie-session' ); 
 
 require('./models/user');
 require('./services/passport');
@@ -9,7 +11,17 @@ mongoose.connect( keys.mongoURI, () => {
     console.log("connected!!");
 } );
 
+
 const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [ keys.cookieKey ]
+    })
+);
+
+app.use( passport.initialize() );
+app.use( passport.session() );
 
 // middlewares
 // var authRoutes = require('./routes/authRoutes') and then authRoutes( app ) also works        ;
