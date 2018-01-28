22877b25c322676ae01cd8a32956a688440856e7
kinny94 <ad.sam007@gmail.com>
2018-01-28 18:42:15 -0500

    added react routers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>Survey New</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = ( ) => {
     return (
         <div>
-            Hi! There
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact={ true } component={ Landing }/>
+                    <Route path="/surveys" component={ Dashboard }/>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
