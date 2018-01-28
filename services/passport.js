9165a03646709cb17b9e0daf577a1f8fd07ee2f0
kinny94 <ad.sam007@gmail.com>
2018-01-27 21:53:02 -0500

    Added react with its own server and concurrently

--- services/passport.js
+++ services/passport.js
@@ -20,8 +20,7 @@ passport.use(
     {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
+        callbackURL: '/auth/google/callback'
     },
     
     ( accessToken, refreshToken, profile, done ) => {
