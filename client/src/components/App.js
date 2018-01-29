09d23dbd1ed0af0d33ff2883fc3b225a251ddfed
kinny94 <ad.sam007@gmail.com>
2018-01-28 22:08:31 -0500

    Making request with actions creators

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Header from './Header';
+import * as actions from '../actions';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = ( ) => {
+class App extends Component{
 
-    
+    componentDidMount(){
+        this.props.fetchUser();
+    }
 
-    return (
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route path="/" exact={ true } component={ Landing }/>
-                    <Route path="/surveys" exact component={ Dashboard }/>
-                    <Route path="/surveys/new" component={ SurveyNew } />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+    render(){
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route path="/" exact={ true } component={ Landing }/>
+                        <Route path="/surveys" exact component={ Dashboard }/>
+                        <Route path="/surveys/new" component={ SurveyNew } />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }    
 };
 
-export default App;
+export default connect( null, actions )( App );
