a2cc38dd4973ae54189fc40bba973312719deedc
kinny94 <ad.sam007@gmail.com>
2018-01-28 21:28:18 -0500

    Header design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
     render(){
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#" className="brand-logo left">Logo</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="sass.html">Login with Google</a></li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
