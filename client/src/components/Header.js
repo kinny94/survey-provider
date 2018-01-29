ed27397f5e5c708c6e8a66f29c435f8828df483b
kinny94 <ad.sam007@gmail.com>
2018-01-28 22:31:42 -0500

    changing header content with routes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+    renderContent(){
+        switch( this.props.auth ){
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href="/auth/google">Login with Google</a></li>
+                );
+            default:
+                return (
+                    <li><a href="/api/logout">Logout</a></li>   
+                );
+        }
+    }
+
     render(){
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a href="#" className="brand-logo">Logo</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">JavaScript</a></li>
+                        { this.renderContent() }
                     </ul>
                 </div>
             </nav>
@@ -17,4 +32,10 @@ class Header extends Component {
     }
 }
 
-export default Header;
+const mapsStateToProps = ( state ) => {
+    return {
+        auth: state.auth
+    };
+}
+
+export default connect( mapsStateToProps )( Header );
