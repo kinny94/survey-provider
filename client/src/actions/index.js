dc2a617e03ae58454aabe4e4c281e6569abf8b1f
kinny94 <ad.sam007@gmail.com>
2018-01-28 22:18:09 -0500

    refactored with async awair

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 export const fetchUser = () => {
-    return function( dispatch ){
-        axios.get( '/api/current_user')
-        .then( res => dispatch( { type: FETCH_USER, payload: res }));
+    return async function( dispatch ){
+        const res = await axios.get( '/api/current_user');
+        dispatch( { type: FETCH_USER, payload: res.data });
     }
 };
 
