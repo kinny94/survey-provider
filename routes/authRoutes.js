1a82333f53fdbba3a44238736d126e684ca57294
kinny94 <ad.sam007@gmail.com>
2018-01-28 22:36:56 -0500

    Redirecting on login logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,14 +7,13 @@ module.exports = ( app ) => {
 		}) 
 	);
 
-	app.get( '/auth/google/callback', passport.authenticate( 'google', {
-		
-		}) 
-	);
+	app.get( '/auth/google/callback', passport.authenticate( 'google' ), ( req, res ) => {
+		res.redirect( "/surveys" );
+	});
 
 	app.get( '/api/logout', ( req, res ) => {
 		req.logout();	// automatically given by passport - kills all the cookies and id inside  the request
-		res.send( req.user );
+		res.redirect( "/" );
 	});
 
 	app.get( '/api/current_user', ( req, res ) => {
