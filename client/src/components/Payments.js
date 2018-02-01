29f9e4d0a0ee2bd09deb88a65ae5b67899ec97b7
kinny94 <ad.sam007@gmail.com>
2018-02-01 00:59:38 -0500

    Added key

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,16 @@ class Payments extends Component{
     render(){
         return(
             <StripeCheckout 
+                name="Survey Provider"
+                description="$5 for 5 surveys"
                 amount={ 500 }
                 token={ token => console.log( token )}
                 stripeKey={ process.env.REACT_APP_STRIPE_KEY }
-            />
+            >
+                <button className="btn">
+                    Add Credits
+                </button>
+            </StripeCheckout>
         );
     }
 }
