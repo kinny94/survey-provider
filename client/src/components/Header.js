d999fa571efb29b43790c0eba474afceab391754
kinny94 <ad.sam007@gmail.com>
2018-02-01 00:54:30 -0500

    Added stripe UI

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
 
@@ -13,9 +14,10 @@ class Header extends Component {
                     <li><a href="/auth/google">Login with Google</a></li>
                 );
             default:
-                return (
+                return [
+                    <li><Payments /></li>,
                     <li><a href="/api/logout">Logout</a></li>   
-                );
+                ];
         }
     }
 
--- client/src/index.js
+++ client/src/index.js
@@ -17,5 +17,5 @@ ReactDOM.render(
     document.querySelector( '#root' )
 );
 
-console.log( 'Stripe key is ', process.env.REACT_APP_STRIPE_KEY );
-console.log( 'Environment is ', process.env.NODE_ENV );
+// console.log( 'Stripe key is ', process.env.REACT_APP_STRIPE_KEY );
+// console.log( 'Environment is ', process.env.NODE_ENV );
