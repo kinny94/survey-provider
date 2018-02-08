4a0cb103a440588ae2fa558ea542ae64ee019682
kinny94 <ad.sam007@gmail.com>
2018-02-08 01:12:37 -0500

    handled survey routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,12 @@ const surveyTemplate = require( '../services/emailTemplates/surveyTemplate' );
 const Survey = mongoose.model( 'surveys' );
 
 module.exports = app => {
-    app.post( '/api/surveys', requireLogin, requireCredits, ( req, res ) => {
+
+    app.get( '/api/surveys/thanks', ( req, res ) => {
+        res.send( 'Thanks for voting!' );    
+    });
+
+    app.post( '/api/surveys', requireLogin, requireCredits, async ( req, res ) => {
         const { title, subject, body, recipients } = req.body; 
 
         const survey = new Survey({
@@ -21,7 +26,18 @@ module.exports = app => {
 
         // great place to send an email
         const mailer = new Mailer( survey, surveyTemplate( survey ));
-        mailer.send();
-
+        
+        try{
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            
+            res.send( user );
+        }
+        catch( err ){
+            res.status( 422 ).send( err );
+        }
+        
     });
 };  
