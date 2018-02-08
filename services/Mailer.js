7b991a7a4f0aa7372ec2d6822d4479c306eec10d
kinny94 <ad.sam007@gmail.com>
2018-02-07 21:54:55 -0500

    added sendgrid stuff

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,12 +3,43 @@ const helper = sengrid.mail;
 const keys = require( '../config/keys' );
 
 class Mailer extends helper.Mail {
+    
     constructor( { subject, recipients }, content ){
         super();
         this.from_email = new helper.Email( 'no-reply@surveyprovider.com' );
         this.subject = subject;
         this.body = new helper.Content( 'text/html', content );
         this.recipients = this.formatAddresses( recipients );  
+    
+        this.addContent( this.body );
+        this.addClickTracking(); 
+        this.addRecipients();
+    }
+
+    formatAddresses( recipients ){
+        return recipients.map(({ email }) => {
+            return new helper.Email( email );
+        });
+    }
+
+    addClickTracking(){
+
+        const trackSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking( true, true );
+
+        trackSettings.setClickTracking( clickTracking );
+        this.addTrackingSettings( trackSettings ); 
+    
+    }
+
+    addRecipients(){
+
+        const personalize = new helper.Personalization();
+        this.recipients.forEach( recipient => {
+            personalize.addTo( recipient );
+        });
+        this.addPersonalization( personalize );
+    
     }
 }
 
