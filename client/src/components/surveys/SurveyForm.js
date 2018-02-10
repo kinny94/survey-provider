c382312a1b6ae52514a48653d7c16ea0e666a823
kinny94 <ad.sam007@gmail.com>
2018-02-09 20:18:41 -0500

    Added form buttons and link

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
-
+import { Link } from 'react-router-dom';
 const FIELDS = [
     { label: 'Survey Title', name: 'title' },
     { label: 'Subject Line', name: 'Subject' },
@@ -23,7 +23,12 @@ class SurveyForm extends Component{
             <div>
                 <form onSubmit={ this.props.handleSubmit( values => console.log( values ))}> 
                     { this.renderFields() }
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text" >
+                        Cancel <i className="material-icons right">cancel</i>
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
