d3383e4aa3d3f6a412ac8f8450dc8f8dd9f83e61
kinny94 <ad.sam007@gmail.com>
2018-02-12 22:05:32 -0500

    Added webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -40,4 +40,9 @@ module.exports = app => {
         }
         
     });
+
+    app.post( '/api/surveys/webhooks', ( req, res ) => {
+        console.log( req.body );
+        res.send({});
+    });
 };  
