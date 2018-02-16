94a841d4368e933da0b361859e56197b4e8c2443
kinny94 <ad.sam007@gmail.com>
2018-02-15 20:39:17 -0500

    added webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -47,24 +47,32 @@ module.exports = app => {
 
     app.post( '/api/surveys/webhooks', ( req, res ) => {
         
-        const p = new Path( '/api/surveys/:surveyId/:choice' );
+    const p = new Path( '/api/surveys/:surveyId/:choice' );
         
-    const events = _.chain( req.body )
+    _.chain( req.body )
         .map(({ email, url }) => {
             
-                const match = p.test( new URL( url ).pathname );
+            const match = p.test( new URL( url ).pathname );
 
-                if( match ){
-                    return { email: email, surveyId: match.surveyId, choice: match.choice };   
-                }
-            })
+            if( match ){
+                return { email: email, surveyId: match.surveyId, choice: match.choice };   
+            }
         })
-
-        .compact( events )
-        .uniqBy( compactEvents, 'email', 'surveyId' )
+        .compact()
+        .uniqBy( 'email', 'surveyId' )
+        .each( event => {
+            Survey.updateOne({
+                _id: surveyId,
+                recipients: {
+                    $elemMatch: { email: email, responded: false }
+                }
+            }, {
+                $inc: { [ choice ]: 1 },
+                $set: { 'recipients.$.responded': 'true'}
+            }).exec();
+        })
         .value();
 
-
-        console.log( events );
         res.send({});
-};  
+    });
+}
