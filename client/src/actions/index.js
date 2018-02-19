4b4d82d6aee42de2ccdda2894c552cc79a7da146
kinny94 <ad.sam007@gmail.com>
2018-02-18 20:32:59 -0500

    addded action and reducers for detching surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => {
     return async function( dispatch ){
@@ -22,5 +22,9 @@ export const submitSurvey = ( values, history ) => async dispatch => {
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get( '/api/surveys' );
 
+    dispatch({ type: FETCH_SURVEYS, payload: res.data });
+};
 
