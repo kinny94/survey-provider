cfd5eee9f2d5c4d34a7ba8141727c8cecca694d0
kinny94 <ad.sam007@gmail.com>
2018-02-18 20:27:44 -0500

    Added surveys implementations for backend

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,79 +1,88 @@
-const _ = require( 'lodash' ); // library for different functions
-const Path = require( 'path-parser' );
-const { URL } = require( 'url' );   
-const mongoose = require( 'mongoose' );
+    const _ = require( 'lodash' ); // library for different functions
+    const Path = require( 'path-parser' );
+    const { URL } = require( 'url' );   
+    const mongoose = require( 'mongoose' );
 
-const requireLogin =  require( '../middlewares/requireLogin' );
-const requireCredits = require( '../middlewares/requireCredits' );
-const Mailer = require( '../services/Mailer' );
-const surveyTemplate = require( '../services/emailTemplates/surveyTemplate' );
+    const requireLogin =  require( '../middlewares/requireLogin' );
+    const requireCredits = require( '../middlewares/requireCredits' );
+    const Mailer = require( '../services/Mailer' );
+    const surveyTemplate = require( '../services/emailTemplates/surveyTemplate' );
 
-const Survey = mongoose.model( 'surveys' );
+    const Survey = mongoose.model( 'surveys' );
 
-module.exports = app => {
+    module.exports = app => {
 
-    app.get( '/api/surveys/:surveyId/:choice', ( req, res ) => {
-        res.send( 'Thanks for voting!' );    
-    });
 
-    app.post( '/api/surveys', requireLogin, requireCredits, async ( req, res ) => {
-        const { title, subject, body, recipients } = req.body; 
+        app.get( '/api/surveys', requireLogin, async ( req, res ) => {
+            const surveys = await Survey.find({ _user: req.user.id }).select({ recipients: false });
 
-        const survey = new Survey({
-            title, 
-            subject,
-            body,
-            recipients: recipients.split( ',' ).map( email => ({ email: email.trim() })),
-            _user: req.user.id,
-            dateSent: Date.now()    
+            res.send( surveys );
         });
 
-        // great place to send an email
-        const mailer = new Mailer( survey, surveyTemplate( survey ));
-        
-        try{
-            await mailer.send();
-            await survey.save();
-            req.user.credits -= 1;
-            const user = await req.user.save();
-            
-            res.send( user );
-        }
-        catch( err ){
-            res.status( 422 ).send( err );
-        }
-        
-    });
+        app.get( '/api/surveys/:surveyId/:choice', ( req, res ) => {
+            res.send( 'Thanks for voting!' );    
+        });
 
-    app.post( '/api/surveys/webhooks', ( req, res ) => {
-        
-    const p = new Path( '/api/surveys/:surveyId/:choice' );
-        
-    _.chain( req.body )
-        .map(({ email, url }) => {
-            
-            const match = p.test( new URL( url ).pathname );
+        app.post( '/api/surveys', requireLogin, requireCredits, async ( req, res ) => {
+            const { title, subject, body, recipients } = req.body; 
+            const rec = recipients.split(',');
+            console.log( rec );
 
-            if( match ){
-                return { email: email, surveyId: match.surveyId, choice: match.choice };   
+            const survey = new Survey({
+                title, 
+                subject,
+                body,
+                recipients: recipients.split( ',' ).map(email => { return { email: email }}),
+                _user: req.user.id,
+                dateSent: Date.now()    
+            });
+
+            // great place to send an email
+            const mailer = new Mailer( survey, surveyTemplate( survey ));
+            
+            try{
+                await mailer.send();
+                await survey.save();
+                req.user.credits -= 1;
+                const user = await req.user.save();
+                
+                res.send( user );
             }
-        })
-        .compact()
-        .uniqBy( 'email', 'surveyId' )
-        .each( ({ surveuId, email, choice }) => {
-            Survey.updateOne({
-                _id: surveyId,
-                recipients: {
-                    $elemMatch: { email: email, responded: false }
-                }
-            }, {
-                $inc: { [ choice ]: 1 },
-                $set: { 'recipients.$.responded': 'true'},
-                lastResponded: new Date()
-            }).exec();
-        })
-        .value();
+            catch( err ){
+                res.status( 422 ).send( err );
+            }
+            
+        });
 
-        res.send({});
-    });
-}
+        app.post( '/api/surveys/webhooks', ( req, res ) => {
+            
+        const p = new Path( '/api/surveys/:surveyId/:choice' );
+            
+        _.chain( req.body )
+            .map(({ email, url }) => {
+                
+                const match = p.test( new URL( url ).pathname );
+
+                if( match ){
+                    return { email: email, surveyId: match.surveyId, choice: match.choice };   
+                }
+            })
+            .compact()
+            .uniqBy( 'email', 'surveyId' )
+            .each( ({ surveuId, email, choice }) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, responded: false }
+                    }
+                }, {
+                    $inc: { [ choice ]: 1 },
+                    $set: { 'recipients.$.responded': 'true'},
+                    lastResponded: new Date()
+                }).exec();
+            })
+            .value();
+
+            res.send({});
+        });
+    }
